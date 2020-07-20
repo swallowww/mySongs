456d2a432a17c209d0eb7e9a7770498f79c5d48e
wenxiaoyan <1105047416@qq.com>
2020-07-20 12:01:49 +0800

    vue-router修改title

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,20 @@ Vue.use(VueRouter)
 import Home from '../pages/home.vue'
 
 const router = new VueRouter({
-    routes: [
-        { path: '/', component: Home }
-    ]
+    routes: [{
+        path: '/',
+        component: Home,
+        meta: {
+            title: "北冥有燕-原创音乐存档"
+        },
+    }]
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.title) {
+        document.title = to.meta.title
+    }
+    next()
 })
 
 export default router;
